3f692a5aa26138e8f939dc933b472c14b3cc4b0b
Guad <rockeurp@gmail.com>
2019-02-28 12:08:16 +0100

    Better healthcheck reporting

--- news/main.py
+++ news/main.py
@@ -7,17 +7,32 @@ class DirectNews(object):
     def on_get(self, req, resp):
         data = stream.cachedNews
 
+        if data == None:
+            resp.body = json.dumps({'message':'Cache sin actualizar. Vuelva a intentar más tarde.'}, ensure_ascii=False)
+            resp.status = falcon.HTTP_503
+            return
+
         resp.body = json.dumps(data, ensure_ascii=False)
 
 class DirectEvents(object):
     def on_get(self, req, resp):        
         data = stream.cachedEvents
 
+        if data == None:
+            resp.body = json.dumps({'message':'Cache sin actualizar. Vuelva a intentar más tarde.'}, ensure_ascii=False)
+            resp.status = falcon.HTTP_503
+            return
+
         resp.body = json.dumps(data, ensure_ascii=False)
 
 class DirectAvisos(object):
     def on_get(self, req, resp):        
         data = stream.cachedAvisos
+
+        if data == None:
+            resp.body = json.dumps({'message':'Cache sin actualizar. Vuelva a intentar más tarde.'}, ensure_ascii=False)
+            resp.status = falcon.HTTP_503
+            return
 
         resp.body = json.dumps(data, ensure_ascii=False)
 
